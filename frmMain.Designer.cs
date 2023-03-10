ecd3bc6529e28a4252216ff4df3994c8e0786de8
orcun.ten <58844296+oten52@users.noreply.github.com>
2023-03-10 14:28:32 +0300

    master
    --

---
+++ frmMain.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QRCodeGenerater
+{
+    partial class frmMain
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.pbOutput = new System.Windows.Forms.PictureBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Input :";
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(59, 10);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(383, 23);
+            this.txtInput.TabIndex = 1;
+            // 
+            // pbOutput
+            // 
+            this.pbOutput.Location = new System.Drawing.Point(59, 76);
+            this.pbOutput.Name = "pbOutput";
+            this.pbOutput.Size = new System.Drawing.Size(383, 275);
+            this.pbOutput.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOutput.TabIndex = 2;
+            this.pbOutput.TabStop = false;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(359, 39);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(83, 23);
+            this.btnGenerate.TabIndex = 3;
+            this.btnGenerate.Text = "Generate QR";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 369);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.pbOutput);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "QR Example";
+            ((System.ComponentModel.ISupportInitialize)(this.pbOutput)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.PictureBox pbOutput;
+        private System.Windows.Forms.Button btnGenerate;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,54 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace QRCodeGenerater
+{
+    /// <summary>
+    /// Autor : Orçun TEN
+    /// Mail : oten52@gmai.com
+    /// </summary>
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        // btnGenerate_Click - QR oluşturma Click Event
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+
+            if (String.IsNullOrEmpty(txtInput.Text))
+            {
+                MessageBox.Show("Input is empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode(txtInput.Text.Trim(), QRCodeGenerator.ECCLevel.Q);
+            QRCode qrCode = new QRCode(qrCodeData);
+
+            var img = new Bitmap(QRCodeGenerater.Properties.Resources.smile);
+            //var localImg = (Bitmap)Bitmap.FromFile("C:\\smile.png");
+
+            // Düz QR oluşturur
+            //Bitmap qrCodeImage = qrCode.GetGraphic(20);
+
+            // QR'ın ortasına resim yerleştirir
+            Bitmap qrCodeImage = qrCode.GetGraphic(20, Color.Black, Color.White, img);
+
+            pbOutput.Image = qrCodeImage;
+        }
+
+    }
+}
